ffabbed2e33c97a6ead6adec1d7cc960f3dc12ff
nhoangvietcuong <sitinhboi782@gmail.com>
2025-05-03 10:59:39 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,8 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),
-    tailwindcss(),
-  ],
+  plugins: [react(), tailwindcss()],
+  define: {
+    global: 'window', // Định nghĩa global là window trong môi trường trình duyệt
+  },
 })
