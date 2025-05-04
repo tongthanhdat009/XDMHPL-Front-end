f0ff9f042b604fbd02f98d3f1b33a2c0867f4adf
htuan2800 <huynhngoctuan48@gmail.com>
2025-05-04 13:58:31 +0700

    bỏ icon

--- src/components/HomeRight/HomeRight.jsx
+++ src/components/HomeRight/HomeRight.jsx
@@ -127,8 +127,8 @@ const HomeRight = () => {
       <div className='flex justify-between items-center text-gray-500 mb-5'>
           <h2 className='text-xl'>Contacts</h2>
           <div className='flex space-x-2'>
-              <SearchRoundedIcon className='h-6' />
-              <MoreHorizRoundedIcon className='h-6' />
+              {/* <SearchRoundedIcon className='h-6' /> */}
+              {/* <MoreHorizRoundedIcon className='h-6' /> */}
           </div>
       </div>
 
